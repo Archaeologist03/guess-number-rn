4932e170319dcce98b5b8829bad0c19b23b80e0a
Archaeologist03 <omniiknight@hotmail.com>
2020-02-04 17:38:58 +0100

    Lecture 22 - custom fonts

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -3,87 +3,87 @@ import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
+import DefaultStyles from '../constants/default-styles';
 
 const generateRandomBetween = (min, max, exclude) => {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	const rndNum = Math.floor(Math.random() * (max - min) + min);
-
-	if (rndNum === exclude) {
-		return generateRandomBetween(min, max, exclude);
-	} else {
-		return rndNum;
-	}
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
 };
 
 const GameScreen = props => {
-	const [currentGuess, setCurrentGuess] = useState(
-		generateRandomBetween(1, 100, props.userChoice),
-	);
-	const [rounds, setRounds] = useState(0);
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice)
+  );
+  const [rounds, setRounds] = useState(0);
+  const currentLow = useRef(1);
+  const currentHigh = useRef(100);
 
-	const currentLow = useRef(1);
-	const currentHigh = useRef(100);
+  const { userChoice, onGameOver } = props;
 
-	const { userChoice, onGameOver } = props;
+  useEffect(() => {
+    if (currentGuess === userChoice) {
+      onGameOver(rounds);
+    }
+  }, [currentGuess, userChoice, onGameOver]);
 
-	useEffect(() => {
-		if (currentGuess === props.userChoice) {
-			props.onGameOver(rounds);
-		}
-	}, [currentGuess, userChoice, onGameOver]);
+  const nextGuessHandler = direction => {
+    if (
+      (direction === 'lower' && currentGuess < props.userChoice) ||
+      (direction === 'greater' && currentGuess > props.userChoice)
+    ) {
+      Alert.alert("Don't lie!", 'You know that this is wrong...', [
+        { text: 'Sorry!', style: 'cancel' }
+      ]);
+      return;
+    }
+    if (direction === 'lower') {
+      currentHigh.current = currentGuess;
+    } else {
+      currentLow.current = currentGuess;
+    }
+    const nextNumber = generateRandomBetween(
+      currentLow.current,
+      currentHigh.current,
+      currentGuess
+    );
+    setCurrentGuess(nextNumber);
+    setRounds(curRounds => curRounds + 1);
+  };
 
-	const nextGuessHandler = direction => {
-		if (
-			(direction === 'lower' && currentGuess < props.userChoice) ||
-			(direction === 'greater' && currentGuess > props.userChoice)
-		) {
-			Alert.alert("Don't lie!", 'You know that this iis wrong...', [
-				{ text: 'Sorry!', style: 'cancel' },
-			]);
-			return;
-		}
-		if (direction === 'lower') {
-			currentHigh.current = currentGuess;
-		} else {
-			currentLow.current = currentGuess;
-		}
-		const nextNumber = generateRandomBetween(
-			currentLow.current,
-			currentHigh.current,
-			currentGuess,
-		);
-		setCurrentGuess(nextNumber);
-		setRounds(currRounds => currRounds + 1);
-	};
-
-	return (
-		<View style={styles.screen}>
-			<Text>Opponent's Guess</Text>
-			<NumberContainer>{currentGuess}</NumberContainer>
-			<Card style={styles.buttonContainer}>
-				<Button title='LOWER' onPress={nextGuessHandler.bind(this, 'lower')} />
-				<Button
-					title='GREATER'
-					onPress={nextGuessHandler.bind(this, 'greater')}
-				/>
-			</Card>
-		</View>
-	);
+  return (
+    <View style={styles.screen}>
+      <Text style={DefaultStyles.title}>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="LOWER" onPress={nextGuessHandler.bind(this, 'lower')} />
+        <Button
+          title="GREATER"
+          onPress={nextGuessHandler.bind(this, 'greater')}
+        />
+      </Card>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	screen: {
-		flex: 1,
-		padding: 10,
-		alignItems: 'center',
-	},
-	buttonContainer: {
-		flexDirection: 'row',
-		justifyContent: 'space-around',
-		marginTop: 20,
-		width: 300,
-		maxWidth: '80%',
-	},
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+    width: 300,
+    maxWidth: '80%'
+  }
 });
+
 export default GameScreen;
